fbea5b32836e5054a1018e7889e61efcd9a8789a
LeToze <antonio.joser.mendes@gmail.com>
2025-04-22 23:24:49 +0100

    Final Updates

--- Logs Fetcher/logsFetcher.py
+++ Logs Fetcher/logsFetcher.py
@@ -3,15 +3,19 @@ import threading
 import os
 from datetime import datetime
 
+#Change for your credentials for ssh, these ones are just for show.
 USERNAME = "cisco"
 PASSWORD = "cisco"
 
 SSH_OPTS = "-oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc"
 
+#CHANGE THIS IF YOU WANT THE FOLDER WITH ANY OTHER NAME OR REMOVE THIS IF ON ROOT
+#IF you do not create the script with create for you dont worry.
 OUTPUT_DIR = "configs"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def log(msg):
+    #Can remove timestamps if time is not important for you.
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {msg}")
 
 def get_running_config(ip):
@@ -39,6 +43,7 @@ def get_running_config(ip):
         filename = os.path.join(OUTPUT_DIR, f"{hostname}_running_config.txt")
         with open(filename, "w") as f:
             f.write(config_output)
+            
 
         log(f"[>>] Saved config from {ip} as {filename}")
 
@@ -49,6 +54,7 @@ def get_running_config(ip):
         log(f"[X] ERROR with {ip}: {e}")
 
 def main():
+    #This must match your txt with you IP address.
     with open("ips.txt", "r") as f:
         ips = [line.strip() for line in f if line.strip()]
 
@@ -61,7 +67,7 @@ def main():
     for t in threads:
         t.join()
 
-    log("[<<] All configs collected. Finishing...")
+    log("[<<] All configs collected. Finished.")
 
 if __name__ == "__main__":
     main()
